0ee47549ae5a45b3f2c4d04ef3d08aa250f75438
Mattia3 <mcortinovis9@gmail.com>
2021-10-12 17:43:00 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,54 +1,32 @@
-
-/*for (let i = 1; i <= 7; i++ ){
-  const row = document.createElement("div");
-  row.classList.add("row");
-  containerScacchiera.append(row);
- 
-  for (let x = 1; x <= 7; x++){
-   
-    const divisibiliPer3 = x % 3 === 0;
-      let bgColor = "";
-
-    if(divisibiliPer3){
-        bgColor = "bg-danger"
-      } 
-      
-    row.innerHTML += `<div class="col g-4">
-                          <div class="ratio ratio-1x1 border ${bgColor}">
-                           <div></div>
-                          </div>
-                      </div>`;
-    
-
-   
-  }
-  
-}*/
-
 for (let i = 1; i <= 100; i++){
   const rowCols = document.querySelector(".row-cols-7")
+  let text = i;
 
+ 
   const divisibiliPer3 = i % 3 === 0;
   let bgColor = "bg-primary";
  
+ 
   if(divisibiliPer3){
-    bgColor = "bg-success"
-    textContent = "fizz" + i;
+    bgColor = "bg-success";
+    text = "fizz";
   } 
 
   const divisibiliper5 = i % 5 === 0;
-  if(divisibiliper5){
-    bgColor = "bg-warning"
+    if(divisibiliper5){
+    bgColor = "bg-warning";
+    text = "buzz";
   }
 
   if(divisibiliPer3 && divisibiliper5){
-    bgColor = "bg-danger"
+    bgColor = "bg-danger";
+    text = "fizzbuzz";
   }
 
  
   rowCols.innerHTML += `<div class="col g-4">
-                          <div class="ratio ratio-1x1 border ${bgColor}">
-                           <div></div>
+                          <div class="ratio ratio-1x1  ${bgColor}">
+                           <div class="text-container">${text}</div>
                           </div>
                       </div>`;
 
