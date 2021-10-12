854de0bf4d759c27a68e3ab8f8a89be79061ad4d
Mattia3 <mcortinovis9@gmail.com>
2021-10-12 17:14:34 +0200

    esercizio non completato

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-console.log("Esercizio")
 
-const containerScacchiera = document.getElementById("container-scacchiera");
-
-for (let i = 1; i <= 7; i++ ){
+/*for (let i = 1; i <= 7; i++ ){
   const row = document.createElement("div");
   row.classList.add("row");
   containerScacchiera.append(row);
@@ -26,4 +23,33 @@ for (let i = 1; i <= 7; i++ ){
    
   }
   
+}*/
+
+for (let i = 1; i <= 100; i++){
+  const rowCols = document.querySelector(".row-cols-7")
+
+  const divisibiliPer3 = i % 3 === 0;
+  let bgColor = "bg-primary";
+ 
+  if(divisibiliPer3){
+    bgColor = "bg-success"
+    textContent = "fizz" + i;
+  } 
+
+  const divisibiliper5 = i % 5 === 0;
+  if(divisibiliper5){
+    bgColor = "bg-warning"
+  }
+
+  if(divisibiliPer3 && divisibiliper5){
+    bgColor = "bg-danger"
+  }
+
+ 
+  rowCols.innerHTML += `<div class="col g-4">
+                          <div class="ratio ratio-1x1 border ${bgColor}">
+                           <div></div>
+                          </div>
+                      </div>`;
+
 }
